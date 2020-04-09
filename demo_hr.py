03181a1acd82828f7063d86ff5888efca7c0b8de
zhangheng <zhangheng0101@126.com>
2020-04-09 17:03:00 +0800

    add argparse for demo_hr

--- demo_hr.py
+++ demo_hr.py
@@ -7,13 +7,20 @@ import cv2 as cv
 import pdb
 plt.rcParams['image.cmap'] = 'gist_earth'
 np.random.seed(98765)
-
+import os
+import argparse
 import sys
 sys.path.append("..")
+
 from tf_unet import image_gen
 from tf_unet import unet
 from tf_unet import util
 from tf_unet.image_util import ImageDataProvider, ImageDataSingle
+parser = argparse.ArgumentParser()
+parser.add_argument("-p", "--predict", help="predict only", action="store_true")
+args = parser.parse_args()
+predict_only = args.predict
+print("bool: ", predict_only)
 
 # cmap = {"background":[0,0,0], "lane_line":[128,0,0], "tree":[0,128,0], "tank":[128,128,0], "building":[0,0,128], "plane":[128,0,128], "wzw":[0,128,128]}
 label_cmap = {0:[0,0,0], 1:[128,0,0], 2:[0,128,0], 3:[128,128,0], 4:[0,0,128], 5:[128,0,128], 6:[0,128,128]}
@@ -27,15 +34,15 @@ class_weights = np.ones(data_provider.n_class)*5
 class_weights[0] = 0.5
 net = unet.Unet(data_provider.channels, data_provider.n_class, layers=3,
                 features_root=32, cost_kwargs={"class_weights": class_weights})
-# net = unet.Unet(data_provider.channels, data_provider.n_class, cost="dice_coefficient", layers=3,
-#                 features_root=32, cost_kwargs={"class_weights": class_weights})
 
 # trainer = unet.Trainer(net, optimizer="momentum", batch_size=10, verification_batch_size=3, opt_kwargs=dict(learning_rate=0.02))
 trainer = unet.Trainer(net, optimizer="adam", batch_size=3, verification_batch_size=3, opt_kwargs=dict(learning_rate=0.001))
-path = trainer.train(data_provider, output_path, training_iters=40, epochs=500)
+if not predict_only:
+    path = trainer.train(data_provider, output_path, training_iters=40, epochs=500, restore=True)
+
 # path = trainer.train(data_provider, output_path, training_iters=30, epochs=100)
 
-
+# predict
 def stack_imgs(imgs, num_row, num_col):
     '''
     concatenate image slices to a panoroma,
@@ -45,7 +52,7 @@ def stack_imgs(imgs, num_row, num_col):
     for i in range(num_row):
         imgs_row.append(np.concatenate(imgs[i*num_col:(i+1)*num_col], axis=1))
     img_stack = np.concatenate(imgs_row, axis=0)
-    return img_stack
+    return img_stack.astype(np.uint8)
 
 path = os.path.join(output_path, "model.ckpt")
 images = data_provider.images_origin
@@ -79,23 +86,3 @@ img_add = cv.addWeighted(img, 0.8, pred_rgb, 0.2, 0)
 cv.imwrite("img_add.png", img_add)
 uniq = img_gt == pred_gt
 print("accuracy: %.4f" %(uniq.sum()/np.product(img_gt.shape)))
-
-# img = cv.imread("./data/cj_right_all.png", -1)
-# data = []
-# nx = 572
-# for i in range(0, img.shape[1]-nx, nx):
-#     img_tmp = img[:nx, i:i+nx]
-#     data.append(img_tmp)
-# data = np.array(data)
-
-# prediction = net.predict(path, data)
-# data_crop = util.crop_to_shape(data, prediction.shape)
-# for i, img in enumerate(data_crop):
-#     pred_img = np.zeros_like(img)
-#     for l in range(1,data_provider.n_class):
-#         mask = prediction[i, :, :, l] > 0.1
-#         color = label_cmap[l]
-#         pred_img[mask] = color
-#     img_tmp = cv.addWeighted(img, 0.8, pred_img, 0.2, 0)
-#     cv.imwrite("pred_%d.png" %i, img_tmp)
-#     cv.imwrite("pred_%d.png" %i, pred_img)
